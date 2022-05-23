c8229a5c2ffbd1c139b2888740ea1adccaec1bb2
Edgardo Ortiz <e.ortiz.v@gmail.com>
2022-05-23 19:28:29 +0200

    Updating to v0.8.9
    - Replace `*`s in aminoacid sequences by `X` prior to alignment, MAFFT automatically removes stop codons, breaking our implementation of alignment by codons

--- captus/align.py
+++ captus/align.py
@@ -935,6 +935,10 @@ def collect_sample_markers(
         if Path(destination).parts[-2] in [settings.MARKER_DIRS["DNA"],settings.MARKER_DIRS["CLR"]]:
             seq_no_ns = fasta_in[seq_name_full]["sequence"].replace("n", "-")
             fasta_in[seq_name_full]["sequence"] = seq_no_ns
+        # Replace stop codons by X, MAFFT automatically removes the '*' symbol
+        if Path(destination).parts[-2] == settings.MARKER_DIRS["AA"]:
+            seq_no_stops = fasta_in[seq_name_full]["sequence"].replace("*", "X")
+            fasta_in[seq_name_full]["sequence"] = seq_no_stops
         seq_name_parts = seq_name_full.split(settings.SEQ_NAME_SEP)
         marker_name = seq_name_parts[1]
         fasta_out = Path(destination, f"{marker_name}{suffix}")
