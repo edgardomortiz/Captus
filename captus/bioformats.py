514b332a0ecfca3d5e7a263e5cda5c17d48af681
Edgardo Ortiz <e.ortiz.v@gmail.com>
2022-06-03 15:26:27 +0200

    Updating to v0.9.51
    - Small fix to YAML parser
    - Reverting introduced path bug during alignment step

--- captus/bioformats.py
+++ captus/bioformats.py
@@ -1323,7 +1323,7 @@ def scipio_yaml_to_dict(
                             except ValueError:
                                 pass
                         else:
-                            if k == "    target" and " " in v: v = v.strip("'").split()[0]
+                            if k == "    target" and " " in v: v = v.split()[0].strip("'").strip('"')
                             if v.startswith("'"): v = v.strip("'").replace(" ", "")
                             if v.startswith("["): v = [int(n) for n in v[1:-1].split(", ")]
                         if k == "  - ID":
